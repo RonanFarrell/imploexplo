f1b15c0b2b49ae67d005ff421e2a4ec7735ea985
Ronan Farrell <rrrfarrell@gmail.com>
2013-11-11 15:32:19 +0000

    removed stats.js

--- js/main.js
+++ js/main.js
@@ -14,23 +14,11 @@ window.requestAnimFrame = (function(){
 };
 })();
 
-var stats = new Stats();
-stats.setMode(0); // 0: fps, 1: ms
-
-// Align top-left
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-
-document.body.appendChild( stats.domElement );
-
 function run()
 {
-	stats.begin();
     running = true;
     game.update();
     game.draw();
-  stats.end();
 
     if (!gameOver) {
 		window.requestAnimFrame(run);
